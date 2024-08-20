651411c9a81d97a9fb90275f74798b5af41f3efa
Modi08 <indiabestgo@gmail.com>
2024-08-20 17:43:02 +0200

    made app compatible with android

--- android/app/src/main/kotlin/com/example/nearmessageapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/nearmessageapp/MainActivity.kt
@@ -1,5 +1,38 @@
 package com.example.nearmessageapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.app.ActivityCompat
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+
+    private var requestCode = 100;
+    private lateinit var  requestPermissionLauncher: ActivityResultLauncher<String>;
+    private var Location_Permission = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
+    );
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        val channel = "hotzone/locationPermission"
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel).setMethodCallHandler { call, result ->
+            when(call.method) {
+                "getLocationPermission" -> {
+                    if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        result.success(2)
+                    } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission_group.LOCATION)){
+                        result.success(1);
+                    } else {
+                        ActivityCompat.requestPermissions(this, Location_Permission, requestCode)
+                        result.success(0);
+                    }
+                }
+            }
+        }
+    }
+}
